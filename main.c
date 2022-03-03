229bff424eab8dc51e6df1e7daf2ebf11a091341
karol0346 <100830715+karol0346@users.noreply.github.com>
2022-03-03 08:25:26 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+#define pi 3.14 
+
+int main(void) {
+  int r=2;
+  double Pole = r*r*pi;
+  printf("%f", Pole);//dowiedzieć się na czym polega funkcja scanf i jak jej używać (wprowadzanie danych)
+  return 0;//zrobić 3 pierwsze zadania
+}
